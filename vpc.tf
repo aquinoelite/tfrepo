6b1b70d0d3951bac3558c82a14abe549d6d438b3
Noelito A Aquino <noelito.aquino@ft.com>
2025-02-05 15:20:35 +0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "tfvpcnoel" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
@@ -11,8 +26,10 @@ resource "aws_vpc" "tfvpcnoel" {
 resource "aws_subnet" "tfsubnet" {
   vpc_id                  = aws_vpc.tfvpcnoel.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-1a"
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
+
+  depends_on = [aws_vpc.tfvpcnoel]
 
   tags = {
     Name = "tfsubnet"
