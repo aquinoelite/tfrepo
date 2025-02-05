a265de4884e2e32a280513cd716034d23be6d0b5
Noelito A Aquino <noelito.aquino@ft.com>
2025-02-05 15:53:13 +0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,36 +1,14 @@
-resource "aws_instance" "this" {
+resource "aws_instance" "noel" {
   ami           = "ami-0c614dee691cbbf37"
   instance_type = "t2.micro"
-
-  iam_instance_profile = "FT-EC2-Role"
-
+  key_name      = "devopskeypair"
+  subnet_id     = aws_subnet.subnet1.id
   vpc_security_group_ids = [
-    aws_security_group.this.id
+    aws_security_group.noel.id
   ]
 
-  subnet_id = aws_subnet.public.id
-
-  user_data = <<-EOF
-    #!/bin/bash
-    # Redirect output and errors to a log file for debugging
-    sudo exec > /var/log/user-data.log 2>&1
-    sudo set -x
-
-    # Update all packages
-    sudo dnf update -y
-
-    # Install Nginx
-    sudo dnf install -y nginx
-
-    # Write the HTML content to the Nginx index page
-    sudo echo "<h1>Manila Ops DevOps Training</h1>" > /usr/share/nginx/html/index.html
-
-    # Start and enable the Nginx service
-    sudo systemctl start nginx
-    sudo systemctl enable nginx
-  EOF
-
   tags = {
-    Name = "noel ec2"
+    Name = "noel"
   }
+
 }
