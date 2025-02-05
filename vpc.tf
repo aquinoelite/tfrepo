e6b25ce8b02b4279bb7a97c0d1f8d5826c705ca0
Noelito A Aquino <noelito.aquino@ft.com>
2025-02-05 15:54:42 +0800

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,43 +1,18 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "noel" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
   tags = {
-    Name = "noel-vpc"
+    Name = "noel"
   }
 }
 
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
+resource "aws_subnet" "subnet1" {
+  vpc_id                  = aws_vpc.noel.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
   tags = {
-    Name = "noel-subnet"
+    Name = "subnet1"
   }
-}
-
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "noel-igw"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "noel-public-rt"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
-}
-
-resource "aws_route" "internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.main.id
 }
